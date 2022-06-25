287acbae99d499529fe5859631389fafb65b2a18
Mathias Scheltjens <mathias.scheltjens@gmail.com>
2022-06-25 04:03:21 +0200

    created .env not working

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { store } from "../../store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://wdev2.be/fs_mathias/eindwerk/api/",
+  baseUrl: "https://wdev2.be/fs_mathias/eindwerk/api",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().user.token;
     if (token) {
